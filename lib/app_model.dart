12e41f4b5b6c78a64191902a34ba0e5edcb2dfad
Talion <44683039+talionderhobbit@users.noreply.github.com>
2023-11-11 02:49:21 +0200

    working step counting

---
+++ lib/app_model.dart
@@ -0,0 +1,13 @@
+class AppModel {
+  final String name;
+  final String androidUrl;
+  final String iosUrl;
+  final int requiredSteps;
+
+  AppModel({
+    required this.name,
+    required this.androidUrl,
+    required this.iosUrl,
+    required this.requiredSteps,
+  });
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:dosport/app_model.dart';
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,28 +11,125 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int stepAmount = 0;
+  ShakeDetector? detector;
+
+  List<AppModel> appList = [
+    //Instagram
+    AppModel(
+      name: "Instagram",
+      androidUrl: "com.instagram.and",
+      iosUrl: "19023801823",
+      requiredSteps: 3000,
+    ),
+    //Snapchat
+    AppModel(
+      name: "Snapchat",
+      androidUrl: "com.snapchat.and",
+      iosUrl: "19023801823",
+      requiredSteps: 5000,
+    ),
+    //TiKTok
+    AppModel(
+      name: "TikTok",
+      androidUrl: "com.tiktok.and",
+      iosUrl: "19023801823",
+      requiredSteps: 10000,
+    ),
+  ];
 
   @override
   void initState() {
     super.initState();
+    // Initialize the ShakeDetector
+    detector = ShakeDetector.autoStart(
+      onPhoneShake: () => setState(() => stepAmount += 1000),
+    );
+  }
+
+  @override
+  void dispose() {
+    detector?.stopListening();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        const Text("You did so many steps today:"),
-        Text(stepAmount.toString()),
-        ElevatedButton(
-          onPressed: () async {
-            int steps = stepAmount + 1000;
-            setState(() {
-              stepAmount = steps;
-            });
-          },
-          child: const Text("Get steps"),
+      appBar: AppBar(
+        title: const Text("SportAward"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showLicensePage(
+                context: context,
+                applicationName: "Junction23 \n © QrCheck",
+              );
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "You've walked",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                Text(
+                  "$stepAmount ",
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                Text(
+                  "steps today!",
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: appList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(appList[index].name),
+                  subtitle: Text(
+                    "Walk ${appList[index].requiredSteps} steps to open",
+                  ),
+                  onTap: () {
+                    if (stepAmount >= appList[index].requiredSteps) {
+                      // Open the app
+                    } else {
+                      //Show a snackbar
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 1),
+                          content: Text(
+                            "You need to walk ${appList[index].requiredSteps - stepAmount} more steps to open ${appList[index].name}",
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                );
+              },
+            ),
+          ],
         ),
-      ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            stepAmount = 0; // Reset stepAmount and call setState
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
     );
   }
 }
